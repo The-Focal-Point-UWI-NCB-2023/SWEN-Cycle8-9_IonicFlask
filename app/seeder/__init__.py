93888f86c0ab8f56c04a14661c8109bf0cf1c026
Kieran Jaggernauth <thejugg63@gmail.com>
2023-08-15 04:40:22 -0500

    Completed database seeder

--- app/seeder/__init__.py
+++ app/seeder/__init__.py
@@ -7,12 +7,10 @@ import os
 
 load_dotenv()  # load environment variables from .env if it exists.
 
-# Initialize Faker Library
-fake = Faker()
 user_count = 20
-product_count = 10
-order_count = 10
-line_item_count = 10
+product_count = 20
+order_count = 30
+line_item_count = 30
 
 """
 Function to seed the database
@@ -20,6 +18,9 @@ Function to seed the database
 
 
 def seed_database(app: Flask):
+    # Initialize Faker Library
+    fake = Faker()
+
     with app.app_context():
         if (
             os.environ.get("ENVIRONMENT") == "development"
@@ -39,21 +40,22 @@ def seed_database(app: Flask):
             users = []
             for i in range(user_count):
                 name = fake.name()
-                email = f'{name.join("_")}@{fake.domain_name()}'
-                password = name + email + str(randint(0, 100) * randint(0, 100))
+                email = f'{"_".join(name.split(" ")).lower()}@{fake.domain_name()}'
+                password = "testaccount"
                 users.append(Users(name, email, password, randint(0, 1)))
 
             s.bulk_save_objects(users)
+
+            users = s.query(Users).all()
 
             # Make Products
             products = []
             for i in range(product_count):
                 name = fake.text(max_nb_chars=20)
-                description = fake.paragraph(nb_sentences=5).join(" ")
+                description = "".join(fake.paragraph(nb_sentences=5))
                 price = randint(0, 2000)
                 image = randint(0, 5)
                 status = "published"
-                users = s.query(Users).all()
                 user_id = users[randint(0, user_count - 1)].id
                 products.append(
                     Products(name, description, price, image, status, user_id)
@@ -61,10 +63,11 @@ def seed_database(app: Flask):
 
             s.bulk_save_objects(products)
 
+            products = s.query(Products).all()
+
             # Orders
             orders = []
             for i in range(order_count):
-                users = s.query(Users).all()
                 user_id = users[randint(0, user_count - 1)].id
                 billing_address = fake.address()
                 total_amount = 0
@@ -73,6 +76,17 @@ def seed_database(app: Flask):
                 orders.append(Orders(user_id, billing_address, total_amount, status))
 
             s.bulk_save_objects(orders)
+
+            orders = s.query(Orders).all()
+
+            # Line Items
+            line_items = []
+            for i in range(line_item_count):
+                product_id = products[randint(0, product_count - 1)].id
+                order_id = orders[randint(0, order_count - 1)].id
+                line_items.append(Line_Items(order_id, product_id, randint(1, 10)))
+
+            s.bulk_save_objects(line_items)
 
             s.commit()
             print("===!!DATABASE SEEDED WITH FAKE DATA!!===")
