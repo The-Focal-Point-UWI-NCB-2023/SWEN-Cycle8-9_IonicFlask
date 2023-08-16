778880c4aae08828e12d9a4067d090cae384aa46
J11an <jaysun1w@gmail.com>
2023-08-16 10:42:47 -0500

    Merge dev into the new api

--- app/api/rest/users.py
+++ app/api/rest/users.py
@@ -1,9 +1,14 @@
+<<<<<<< HEAD
 from app.api import Namespace, Resource, fields, reqparse, abort
 from app.models import Users, db
+=======
+from app.api.rest import Blueprint, jsonify, request, Users, db
+>>>>>>> f689c0636d59411cb9ffd3aa65283696c40aace2
 
 users_ns = Namespace("users", path="/v1/rest/users",
                       description="API operations related to managing user accounts, including creating, retrieving, updating, and deleting user information. This namespace provides endpoints to interact with user data, allowing administrators to manage user profiles and authentication details. Users can be searched by ID, and their full name, email, and role information are accessible for administration purposes.",)
 
+<<<<<<< HEAD
 user_model = users_ns.model(
     "User",
     {
@@ -56,15 +61,94 @@ class UsersResource(Resource):
 class UserResource(Resource):
     @users_ns.marshal_with(user_model)
     def get(self, user_id):
+=======
+
+@users.route("/", methods=["GET"])
+def get_users():
+    try:
+        users = Users.query.all()
+        user_list = [
+            {
+                "id": user.id,
+                "full_name": user.full_name,
+                "email": user.email,
+                "role": user.role,
+            }
+            for user in users
+        ]
+        return jsonify(users=user_list), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@users.route("/<int:user_id>", methods=["GET"])
+def get_role(user_id):
+    user = Users.query(user_id)
+    return jsonify({"user_id": user.id, "role": user.role})
+
+
+@users.route("/", methods=["POST"])
+def create_user():
+    try:
+        full_name = request.form.get("full_name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        role = request.form.get("role")
+
+        if not all([full_name, email, password, role]):
+            return jsonify({"error": "Missing required data"}), 400
+
+        new_user = Users(full_name=full_name, email=email, password=password, role=role)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({"message": "User created successfully"}), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@users.route("/<int:user_id>", methods=["PUT"])
+def update_user(user_id):
+    try:
+>>>>>>> f689c0636d59411cb9ffd3aa65283696c40aace2
         user = Users.query.get(user_id)
         if user:
             return user
         elif user is None:
             abort(404,message='User with this ID does not exist')
 
+<<<<<<< HEAD
     @users_ns.expect(user_parser)
     @users_ns.marshal_with(user_model)
     def put(self, user_id):
+=======
+        if not user:
+            return jsonify({"error": "User not found"}), 404
+
+        data = request.form
+        if "full_name" in data:
+            user.full_name = data["full_name"]
+        if "email" in data:
+            user.email = data["email"]
+        if "password" in data:
+            # hashed_password = generate_password_hash(data['password'], method='pbkdf2:sha256')
+            user.password = data["password"]
+        if "role" in data:
+            user.role = data["role"]
+
+        db.session.commit()
+
+        return jsonify({"message": "User updated successfully"}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+@users.route("/<int:user_id>", methods=["DELETE"])
+def delete_users(user_id):
+    try:
+>>>>>>> f689c0636d59411cb9ffd3aa65283696c40aace2
         user = Users.query.get(user_id)
         if user:
             try:
@@ -78,6 +162,7 @@ class UserResource(Resource):
         elif user is None:
             abort(404,message='User with this ID does not exist')
 
+<<<<<<< HEAD
 
     @users_ns.response(204, "User deleted")
     @users_ns.marshal_with(user_model)
@@ -89,3 +174,15 @@ class UserResource(Resource):
             return "", 204
         elif user is None:
             abort(404,message='User with this ID does not exist')
+=======
+        if not user:
+            return jsonify({"error": "User not found"}), 404
+
+        db.session.delete(user)
+        db.session.commit()
+
+        return jsonify({"message": "User deleted successfully"}), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+>>>>>>> f689c0636d59411cb9ffd3aa65283696c40aace2
--- app/tests/manualTestLists.py
+++ app/tests/manualTestLists.py
@@ -3,7 +3,7 @@ test_orders = [
     [2, 456, "456 Elm St, Town", 75.25, "Shipped"],
     [3, 789, "789 Oak St, Village", 250.00, "Delivered"],
     [4, 123, "321 Pine St, Hamlet", 50.75, "Pending"],
-    [5, 456, "654 Birch St, Countryside", 150.20, "Shipped"]
+    [5, 456, "654 Birch St, Countryside", 150.20, "Shipped"],
 ]
 
 test_products = [
@@ -11,7 +11,7 @@ test_products = [
     Product("Product 2", "Description 2", 25.50, "image2.jpg", "Out of stock", 456),
     Product("Product 3", "Description 3", 150.00, "image3.jpg", "Available", 789),
     Product("Product 4", "Description 4", 75.25, "image4.jpg", "Out of stock", 123),
-    Product("Product 5", "Description 5", 200.75, "image5.jpg", "Available", 456)
+    Product("Product 5", "Description 5", 200.75, "image5.jpg", "Available", 456),
 ]
 
 test_users = [
@@ -19,7 +19,7 @@ test_users = [
     User("Regular User 1", "user1@example.com", "user123", "regular"),
     User("Regular User 2", "user2@example.com", "user456", "regular"),
     User("Admin User 2", "admin2@example.com", "admin456", "admin"),
-    User("Regular User 3", "user3@example.com", "user789", "regular")
+    User("Regular User 3", "user3@example.com", "user789", "regular"),
 ]
 
 order_headers = ["Order ID", "User ID", "Billing Address", "Total Amount", "Status"]
