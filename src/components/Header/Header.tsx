516a05ef56bacb3831717823738938024d4aa39f
Kieran Jaggernauth <thejugg63@gmail.com>
2023-08-19 14:57:45 -0500

    Added navbar with tabs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,18 @@
 import { PropsWithChildren } from 'react'
 import styles from './Header.module.scss'
-import { IonHeader, IonToolbar, IonTitle } from '@ionic/react'
+import {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonIcon,
+} from '@ionic/react'
+import { person, call, settings } from 'ionicons/icons'
 
-const Header: React.FC<PropsWithChildren> = (props) => {
-    return (
-        <IonHeader>
-            <IonToolbar>
-                <IonTitle>Focal Frames</IonTitle>
-            </IonToolbar>
-        </IonHeader>
-    )
+const Header: React.FC<PropsWithChildren> = () => {
+    return <IonHeader></IonHeader>
 }
 
 export default Header
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,24 @@
 // Routing Imports
 import { Redirect, Route } from 'react-router-dom'
-import { IonRouterOutlet } from '@ionic/react'
+import {
+    IonRouterOutlet,
+    IonTabs,
+    IonTabBar,
+    IonTabButton,
+    IonIcon,
+    IonLabel,
+} from '@ionic/react'
 import { IonReactRouter } from '@ionic/react-router'
+
+//Image Imports
+import {
+    glassesOutline,
+    hammerOutline,
+    cartOutline,
+    settingsOutline,
+    logInOutline,
+    logOutOutline,
+} from 'ionicons/icons'
 
 // Pages
 import Landing from '../pages/Landing/Landing'
@@ -9,51 +26,100 @@ import Register from '../pages/Register/Register'
 import Login from '../pages/Login/Login'
 import Products from '../pages/Products/Products'
 import ProductDetails from '../pages/Products/ProductDetails/ProductDetails'
+import Cart from '../pages/Cart/Cart'
 import Admin from '../pages/Admin/Admin'
 import Settings from '../pages/Settings/Settings'
 
 const Router: React.FC = () => {
     //Auth Check
-    const isAuthed = true
+    const isAuthed = false
+    const isAdmin = false
 
     return (
         <IonReactRouter>
-            <IonRouterOutlet>
-                {/* Landing */}
-                <Route exact path="/landing" component={Landing} />
-                <Redirect exact from="/" to="/landing" />
+            <IonTabs>
+                <IonRouterOutlet>
+                    {/* Landing */}
+                    <Route exact path="/landing" component={Landing} />
+                    <Redirect exact from="/" to="/landing" />
 
-                {/* Register */}
-                <Route exact path="/register" component={Register} />
+                    {/* Register */}
+                    <Route exact path="/register" component={Register} />
 
-                {/* Login */}
-                <Route exact path="/login" component={Login} />
+                    {/* Login */}
+                    <Route exact path="/login" component={Login} />
 
-                {/* Products */}
-                <Route exact path="/products" component={Products} />
+                    {/* Products */}
+                    <Route exact path="/products" component={Products} />
 
-                {/* Product Details */}
-                <Route exact path="/products/:id" component={ProductDetails} />
+                    {/* Product Details */}
+                    <Route
+                        exact
+                        path="/products/:id"
+                        component={ProductDetails}
+                    />
 
-                {/* Admin */}
-                <Route
-                    exact
-                    path="/admin"
-                    render={() => {
-                        return isAuthed ? (
-                            <Admin />
-                        ) : (
-                            <Redirect exact from="/" to="/landing" />
-                        )
-                    }}
-                />
+                    {/* Cart */}
+                    <Route exact path="/cart" component={Cart} />
 
-                {/* Settings */}
-                <Route exact path="/settings" component={Settings} />
+                    {/* Admin */}
+                    <Route
+                        exact
+                        path="/admin"
+                        render={() => {
+                            return isAuthed && isAdmin ? (
+                                <Admin />
+                            ) : (
+                                <Redirect exact from="/" to="/landing" />
+                            )
+                        }}
+                    />
 
-                {/* Fallback Route */}
-                <Route render={() => <Redirect to="/landing" />} />
-            </IonRouterOutlet>
+                    {/* Settings */}
+                    <Route exact path="/settings" component={Settings} />
+
+                    {/* Fallback Route */}
+                    <Route render={() => <Redirect to="/landing" />} />
+                </IonRouterOutlet>
+
+                <IonTabBar slot="top">
+                    <IonTabButton tab="products" href="/products">
+                        <IonIcon icon={glassesOutline} />
+                        <IonLabel>Products</IonLabel>
+                    </IonTabButton>
+
+                    <IonTabButton tab="cart" href="/cart">
+                        <IonIcon icon={cartOutline} />
+                        <IonLabel>Cart</IonLabel>
+                    </IonTabButton>
+
+                    {isAuthed && isAdmin ? (
+                        <IonTabButton tab="admin" href="/admin">
+                            <IonIcon icon={hammerOutline} />
+                            <IonLabel>Admin</IonLabel>
+                        </IonTabButton>
+                    ) : (
+                        ''
+                    )}
+
+                    <IonTabButton tab="settings" href="/settings">
+                        <IonIcon icon={settingsOutline} />
+                        <IonLabel>Settings</IonLabel>
+                    </IonTabButton>
+
+                    {isAuthed ? (
+                        <IonTabButton tab="logout" href="/logout">
+                            <IonIcon icon={logOutOutline} />
+                            <IonLabel>Logout</IonLabel>
+                        </IonTabButton>
+                    ) : (
+                        <IonTabButton tab="login" href="/login">
+                            <IonIcon icon={logInOutline} />
+                            <IonLabel>Login</IonLabel>
+                        </IonTabButton>
+                    )}
+                </IonTabBar>
+            </IonTabs>
         </IonReactRouter>
     )
 }
