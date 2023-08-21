4c19740848b7d164524a00e6956f2701de3c5192
Akiel Walsh <71809218+Over-Leveraged@users.noreply.github.com>
2023-08-20 22:11:08 -0500

    Registration fetch
    Registration fetch

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,8 +1,62 @@
+import { useIonToast } from '@ionic/react'
+import { useState } from 'react'
 import Main from '../../components/Main/Main'
 import styles from './Register.module.scss'
 
 const Register: React.FC = () => {
+    const [error, setError] = useState<string>('')
+    const [success, setSuccess] = useState<string>('')
+    const [present] = useIonToast();
+
+    const handleSubmit = async (e: any) => {
+        e.preventDefault();
+        console.log('submitting');
+        try {
+            const form = document.querySelector('#register-form') as HTMLFormElement;
+            const formData = new FormData(form);
+            console.log(formData);
+            fetch('http://localhost:8080/api/v1/auth/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(Object.fromEntries(formData))
+            })
+                .then(async (response) => {
+                    if (response.status === 200) {
+                        const data = await response.json()
+                        console.log(data);
+                        setSuccess(data.message);
+                        present({
+                            message: success,
+                            duration: 3000,
+                            color: "success"
+                        })
+                    } else {
+                        const data = await response.json()
+                        console.log(data);
+                        setError(data.message);
+                        present({
+                            message: error,
+                            duration: 3000,
+                            color: "danger"
+                        })
+                    }
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
     return (
+
+        
+
+
         <Main>
             <h2>Register Page</h2>
         </Main>
