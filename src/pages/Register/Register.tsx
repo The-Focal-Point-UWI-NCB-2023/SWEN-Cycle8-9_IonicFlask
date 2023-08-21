1eac85cbec644af71df91abbd5ae48e3c1c84445
Akiel Walsh <71809218+Over-Leveraged@users.noreply.github.com>
2023-08-20 22:46:35 -0500

    Added Reg form
    Added Reg forms

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,12 +1,42 @@
-import { useIonToast } from '@ionic/react'
-import { useState } from 'react'
+import { IonButton, IonInput, IonItem, IonList, useIonToast } from '@ionic/react'
+import { useState, useEffect } from 'react'
 import Main from '../../components/Main/Main'
 import styles from './Register.module.scss'
+import { use } from 'chai'
 
 const Register: React.FC = () => {
+
     const [error, setError] = useState<string>('')
     const [success, setSuccess] = useState<string>('')
     const [present] = useIonToast();
+    const [csrfToken, setCsrfToken] = useState('');
+
+    async function getCsrfToken() {
+        try {
+            const response = await fetch('http://localhost:8080/api/v1/auth/csrf-token', {
+                method: 'GET',
+                credentials: 'include'
+            })
+            //console.log(response.status);
+            if (response.status === 200) {
+                const data = await response.json()
+                setCsrfToken(data.csrf_token)
+            } else {
+                throw new Error('Failed to fetch CSRF token')
+            }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getCsrfToken();
+    }, []);
+
+    useEffect(() => {
+        console.log("CSRF Token: ", csrfToken);
+    }, []); // This effect will run whenever csrfToken changes
+
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
@@ -55,6 +85,28 @@ const Register: React.FC = () => {
     return (
         <Main>
             <h1>Register</h1>
+            <form id="register-form" onSubmit={handleSubmit}>
+                <div className={styles.formGroup}>
+                <IonList>
+                    <IonItem>
+                        <IonInput name="name" type="text" placeholder="Name" required />
+                    </IonItem>
+                    <IonItem>
+                        <IonInput name="email" type="email" placeholder="Email" required />
+                    </IonItem>
+                    <IonItem>
+                        <IonInput name="password" type="password" placeholder="Password" required />
+                    </IonItem>
+                    <IonItem>
+                        <IonInput name="password_confirmation" type="password" placeholder="Confirm Password" required />
+                    </IonItem>
+                    <IonButton color="primary" onClick={(e) => { handleSubmit(e) }}>Register</IonButton>
+                </IonList>
+
+                </div>
+                <p className={styles.register}>Already have an account? <a href='/login'>Login</a></p>
+            </form>
+                    
         </Main>
     )
 }
