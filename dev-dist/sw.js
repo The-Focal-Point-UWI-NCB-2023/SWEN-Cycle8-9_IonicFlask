337736e2315194f8996d7cd2b41466a5deabe40a
Kieran Jaggernauth <thejugg63@gmail.com>
2023-08-22 20:41:09 -0500

    Removed dist-dev from gitignore

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3muumu1st48"
+    "revision": "0.0dm2qmk38qg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
